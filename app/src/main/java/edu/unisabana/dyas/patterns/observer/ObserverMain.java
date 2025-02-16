629c7ca7f49ba8b1b80fb7eea8ded4fd55ee5b93
Jorge Esteban Diaz Bernal <157375038+JorEstDiBe@users.noreply.github.com>
2025-02-15 21:17:36 -0500

    Fix Program work flow

--- app/src/main/java/edu/unisabana/dyas/patterns/observer/ObserverMain.java
+++ app/src/main/java/edu/unisabana/dyas/patterns/observer/ObserverMain.java
@@ -2,54 +2,115 @@ package edu.unisabana.dyas.patterns.observer;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
 import edu.unisabana.dyas.patterns.observer.impl.ConfigurationManager;
 import edu.unisabana.dyas.patterns.observer.impl.observers.DateFormatObserver;
 import edu.unisabana.dyas.patterns.observer.impl.observers.MoneyFormatObserver;
 
 /**
- * Ejemplo práctico del patrón Observer.
- * Se simula la actualización de la configuración del sistema y se notifica a los observadores.
- * 
- * Nota: Se asume que la clase ConfigurationManager extiende de java.util.Observable
- * y que los observadores implementan java.util.Observer, mostrando su actualización en consola.
+ * @author cesarvefe
+ 
  */
 public class ObserverMain {
 
-    public static void main(String[] args) {
-        // 1. Obtener la instancia única de ConfigurationManager (Singleton)
+    public static void main(String[] args) 
+    {
         ConfigurationManager conf = ConfigurationManager.getInstance();
 
-        // 2. Configuración inicial (valores por default)
-        conf.setDefaultDateFormat(new SimpleDateFormat("dd/MM/yyy"));
-        conf.setMoneyFormat(new DecimalFormat("##.00"));
-        System.out.println("Congifuracion predeterminada ('Default')");
+         //Se establecen los valores por defecto de los formatos
+         conf.setDefaultDateFormat(new SimpleDateFormat("yyyy/MM/dd"));
+         conf.setMoneyFormat(new DecimalFormat("##.00"));
+         System.out.println("Established configuration");
+         
+         //Se dan de alta lo observadores
+         DateFormatObserver dateFormatObserver = new DateFormatObserver();
+         MoneyFormatObserver moneyFormatObserver = new MoneyFormatObserver();
+         conf.addObserver(dateFormatObserver);
+         conf.addObserver(moneyFormatObserver);
 
-        // 3. Crear y registrar los observadores
-        DateFormatObserver dateFormatObserver = new DateFormatObserver();
-        MoneyFormatObserver moneyFormatObserver = new MoneyFormatObserver();
-        conf.addObserver(dateFormatObserver);
-        conf.addObserver(moneyFormatObserver);
-        System.out.println("Añadir Observadores.\n");
+        Scanner scanner = new Scanner(System.in);
 
-        // 4. Primer cambio de configuración: Se espera que los observadores notifiquen el cambio.
-        System.out.println("Changing configuration - Primer Cambio formato: ");
-        conf.setDefaultDateFormat(new SimpleDateFormat("dd/MM/yyyy"));
-        conf.setMoneyFormat(new DecimalFormat("###,#00.00"));
-        System.out.println();
+        // Ciclo en ejecución para cambios en runtime o tiempo real.
+        while (true) 
+        {
+            System.out.println("----------------------------------");
+            System.out.println("\nSelecciona Cambio de formato:");
+            System.out.println("1. Fecha");
+            System.out.println("2. Moneda");
+            System.out.println("3. Salir");
+            System.out.print("Ingresar opcion: ");
+            int instruccion = scanner.nextInt();
+            scanner.nextLine(); 
 
-        // 5. Segundo cambio de configuración: Se notifica nuevamente a los observadores.
-        System.out.println("Changing configuration - Segundo Cambio formato: ");
-        conf.setDefaultDateFormat(new SimpleDateFormat("MM/yyyy/dd"));
-        conf.setMoneyFormat(new DecimalFormat("###,#00"));
-        
-        // 6. Remover los observadores: A partir de aquí, no se recibirán notificaciones
-        conf.removeObserver(dateFormatObserver);
-        conf.removeObserver(moneyFormatObserver);
-        System.out.println("Observadores Removidos.\n");
+            switch (instruccion) 
+            {
+                case 1: //Opcion de cambio de formato de fecha
+                    System.out.println("---------------------------------");
+                    System.out.println("Selecciona un formato de fecha:");
+                    System.out.println("1. dd/MM/yyyy");
+                    System.out.println("2. yyyy-MM-dd");
+                    System.out.println("3. MM-dd-yyyy");
+                    System.out.print("Ingresa: ");
+                    int FormatoFecha = scanner.nextInt();
+                    scanner.nextLine();
 
-        // 7. Tercer cambio de configuración: No habrá notificación ya que los observadores fueron removidos y se cumple con el formato
-        System.out.println("Changing configuration - Tercer Cambio (No se notifican los observadores ya que cumple con el formato ('Default'): ");
-        conf.setDefaultDateFormat(new SimpleDateFormat("MM/yyyy"));
-        conf.setMoneyFormat(new DecimalFormat("###,##0.00"));
+                    switch (FormatoFecha) 
+                    {
+                        case 1:
+                            conf.setDefaultDateFormat(new SimpleDateFormat("dd/MM/yyyy"));
+                            break;
+                        case 2:
+                            conf.setDefaultDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+                            break;
+                        case 3:
+                            conf.setDefaultDateFormat(new SimpleDateFormat("MM-dd-yyyy"));
+                            break;
+                        default:
+                            System.out.println("Instruccion invalida, intente de nuevo...");
+                            break;
+                    }
+                    break;
+
+                case 2: //Opcion de cambio de formato de dinero
+                    System.out.println("-----------------------------------");
+                    System.out.println("Selecciona un formato de dinero:");
+                    System.out.println("1. ###,#00.00");
+                    System.out.println("2. ###,#00");
+                    System.out.println("3. ###,###.00");
+                    System.out.print("Ingresa: ");
+                    int FormatoMoneda = scanner.nextInt();
+                    scanner.nextLine();
+
+                    switch (FormatoMoneda) 
+                    {
+                        case 1:
+                            conf.setMoneyFormat(new DecimalFormat("###,#00.00"));
+                            break;
+                        case 2:
+                            conf.setMoneyFormat(new DecimalFormat("###,#00"));
+                            break;
+                        case 3:
+                            conf.setMoneyFormat(new DecimalFormat("###,##0.00"));
+                            break;
+                        default:
+                            System.out.println("Instruccion invalida, intente de nuevo...");
+                            break;
+                    }
+                    break;
+
+                default:
+                    System.out.println("Instruccion invalida, intente de nuevo...");
+                    break;
+            }
+            
+            if (instruccion == 3) 
+            {
+                break;  // Salir del ciclo
+            }
+        }
+
+        scanner.close();
+        System.out.println("¡Adios!");
     }
 }
